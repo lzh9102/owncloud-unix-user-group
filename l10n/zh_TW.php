7e7f3623002cd9ad97174504b5c62163d88da7c5
Che-Huai Lin <lzh9102@gmail.com>
2014-04-02 21:23:09 +0800

    Add Traditional Chinese translation

---
+++ l10n/zh_TW.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Written by Che-Huai Lin and placed in the public domain
+ */
+
+$TRANSLATIONS = Array(
+	"pwauth_path" => "pwauth 執行檔路徑",
+	"uid_list" => "允許登入的 UID 清單",
+	"uid_list_original-title" => "可指定多個 UID 範圍，以「;」分隔不同範圍；以「-」分隔上下限數值。例: 1000-1003;1005"
+);
+
+?>
